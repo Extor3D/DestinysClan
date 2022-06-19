266dfa895f86482892132c9923e0f806380d6993
sebastiap <sebastiap@gmail.com>
2022-06-18 21:47:29 -0300

    Dibuja Sprite segun si cae o esta apoyado. Ya no empuja plataformas al caer

--- FallBoys/Scripts/Platform.gd
+++ FallBoys/Scripts/Platform.gd
@@ -3,8 +3,8 @@ extends RigidBody2D
 var rot = (randf() * 3) - 2
 export var max_speed = 5.0
 
-#func _ready():
-	#angular_velocity = rot
+func _ready():
+	angular_velocity = rot
 	
 func _integrate_forces(state):
 	if state.linear_velocity.length()>max_speed:
--- FallBoys/Scripts/Player.gd
+++ FallBoys/Scripts/Player.gd
@@ -1,9 +1,10 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO
+var air_jump = true
 
 export (int) var speed = 100
-export (int) var jump_speed = -200
+export (int) var jump_speed = -2000
 export (int) var gravity = 500
 export (float, 0, 1.0) var friction = 0.1
 export (float, 0, 1.0) var acceleration = 0.55
@@ -16,10 +17,10 @@ func _draw():
 			$AnimatedSprite.flip_h = false
 		else: 
 			$AnimatedSprite.flip_h = true
-		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.animation = "idle"
 
 	if not is_on_floor():
-		$AnimatedSprite.animation = "jump"
+		$AnimatedSprite.animation = "falling"
 
 func _physics_process(delta):
 	move_player(delta)
@@ -38,10 +39,18 @@ func move_player(delta):
 		velocity.x = lerp(velocity.x, 0, friction)
 
 	velocity.y += gravity * delta
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
-			velocity.y = jump_speed
-			
-	velocity.y = clamp(velocity.y, -100000, 100)
+	velocity = move_and_slide(velocity, Vector2.UP,false,4,1.5,false)
+	var grounded = is_on_floor()
 
-	velocity = move_and_slide(velocity, Vector2.UP)
+	if Input.is_action_just_pressed("jump"):
+		if grounded:
+			velocity.y = jump_speed
+			air_jump = true
+		elif air_jump:
+			velocity.y = -400
+			air_jump = false
+			
+	velocity.y = clamp(velocity.y, -400, 100)
+	_draw()
+
+
