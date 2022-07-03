507c07a71e252df3b8258c6ba8c96c91d31dbceb
Extor3D <extor3d@gmail.com>
2022-07-03 20:59:53 -0300

    Subo primer version de identificador de formaciones

--- SpaceshipHarem/Player/Scripts/Player.gd
+++ SpaceshipHarem/Player/Scripts/Player.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-export var starting_ships = 10
+export var starting_ships = 5
 export var softness = 0.1
 export var bias = 0
 var ships : Array
@@ -55,12 +55,16 @@ func _ready():
 	
 func _physics_process(delta):
 	for f in $Formations.get_children():
-		var bodies = f.get_overlapping_bodies()
-		#print(bodies)
-		var formation = arrays_have_same_content(bodies, ships)
-		if formation:
-			print("Formation!")
+		var s = 1 + (starting_ships*0.1 - 0.5 )
+		f.position = get_higher_ship()
+		f.scale = Vector2(s, s)
+		is_formation_done(f)
 		
+func get_higher_ship():
+	if $Ship1.position.y < $Ship2.position.y:
+		return $Ship1.position
+	else:
+		return $Ship2.position
 	
 func calc_next_position(currentPosition: Vector2, chain: Chain):
 	return currentPosition + Vector2(0, (chain.get_node("CollisionShape2D").get_shape().height/2)+2)
@@ -83,9 +87,19 @@ func create_joint(p: Vector2, node_a: NodePath):
 func calculate_position(s: PinJoint2D):
 	return lerp(get_node(s.get_node_a()).position, get_node(s.get_node_b()).position, 0.5)
 	
-func arrays_have_same_content(array1, array2):
-	if array1.size() != array2.size(): return false
+func array_is_included_in_array(array1, array2):
+	#if array1.size() != array2.size(): return false
 	for item in array1:
 		if !array2.has(item): return false
 		if array1.count(item) != array2.count(item): return false
 	return true
+	
+func is_formation_done(formation):
+	var areas = formation.get_overlapping_areas()
+	if array_is_included_in_array(ships, areas):
+		var ship1_in_1 = formation.get_node("End1").overlaps_body($Ship1)
+		var ship1_in_2 = formation.get_node("End2").overlaps_body($Ship1)
+		var ship2_in_1 = formation.get_node("End1").overlaps_body($Ship2)
+		var ship2_in_2 = formation.get_node("End2").overlaps_body($Ship2)
+		if (ship1_in_1 and ship2_in_2) or (ship1_in_2 and ship2_in_1):
+			print("Formation!")
