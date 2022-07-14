8107a9151674c32ee5a9524f2e8cbbb7476e335f
Extor3D <extor3d@gmail.com>
2022-07-14 13:02:48 -0300

    Agrego la posibilidad de pasar variables a la instancia que genera el spawner

--- SpaceshipHarem/LevelComponents/Scripts/EnemyTimer.gd
+++ SpaceshipHarem/LevelComponents/Scripts/EnemyTimer.gd
@@ -1,6 +1,7 @@
 extends Timer
 
 export (PackedScene) var enemy
+export (Dictionary) var enemy_variables
 export (int) var y_center = 180
 export (int) var height = 80
 export (float) var start_time = 0
@@ -16,6 +17,8 @@ func _on_EnemyTimer_timeout():
 	#Create a new enemy in the specified range
 	if enemy != null:
 		var e = enemy.instance()
+		for k in enemy_variables.keys():
+			e.set(k, enemy_variables.get(k))
 		e.global_position = Vector2(get_viewport().size.x + 10, rng.randi_range(y_center - height, y_center + height))
 		$CleanWhenOut.add_child(e)
 
