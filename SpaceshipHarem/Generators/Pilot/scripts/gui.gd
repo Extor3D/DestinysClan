f8c7cd1720fc3c136b7492f7a91053df246ce066
Extor3D <extor3d@gmail.com>
2022-08-19 15:08:29 -0300

    Saco logica de UI a la generacion

--- SpaceshipHarem/Generators/Pilot/scripts/gui.gd
+++ SpaceshipHarem/Generators/Pilot/scripts/gui.gd
@@ -60,6 +60,7 @@ func _randomize():
 	# Si es humano
 	var skin_color = max(randi()%3-1, 0)
 
+	type = TYPES[randi() % TYPES.size()]
 	
 	## Completar
 	var primary_color = randi()%COLORS.size()
@@ -188,14 +189,8 @@ func set_portrait(data: Dictionary):
 	# warning-ignore:shadowed_variable
 	for type in PARTS:
 		portrait.get_node(type).set_sprite(data[type])
-		if !has_node("Panel/ScrollContainer/VBoxContainer/"+type):
-			var t = type.split("/")
-			if t.size()>1 && has_node("Panel/ScrollContainer/VBoxContainer/"+t[t.size()-1]):
-				get_node("Panel/ScrollContainer/VBoxContainer/"+t[t.size()-1]+"/VBoxContainer/HBoxContainer/OptionButton").selected = portrait.get_node(type).sprite
-			continue
-		get_node("Panel/ScrollContainer/VBoxContainer/"+type+"/VBoxContainer/HBoxContainer/OptionButton").selected = portrait.get_node(type).sprite
+
 	portrait.get_node("Hair/Brows").position.y = -data.brows_offset
-	get_node("Panel/ScrollContainer/VBoxContainer/Brows/VBoxContainer/Offset/HSlider").value = data.brows_offset
 	
 	skin_material.set_shader_param("light_color", data.skin_light_color)
 	skin_material.set_shader_param("dark_color", data.skin_dark_color)
@@ -228,23 +223,6 @@ func set_portrait(data: Dictionary):
 	hair_material.set_shader_param("white_color", data.white_color)
 	hair_material.set_shader_param("black_color", data.black_color)
 	
-	$Panel/ScrollContainer/VBoxContainer/Colors/VBoxContainer/Black/ColorPickerButton.color = data.black_color
-	$Panel/ScrollContainer/VBoxContainer/Colors/VBoxContainer/White/ColorPickerButton.color = data.white_color
-	$Panel/ScrollContainer/VBoxContainer/Body/VBoxContainer/LightSkin/ColorPickerButton.color = data.skin_light_color
-	$Panel/ScrollContainer/VBoxContainer/Body/VBoxContainer/DarkSkin/ColorPickerButton.color = data.skin_dark_color
-	$Panel/ScrollContainer/VBoxContainer/Eyes/VBoxContainer/LightEye/ColorPickerButton.color = data.eye_light_color
-	$Panel/ScrollContainer/VBoxContainer/Eyes/VBoxContainer/DarkEye/ColorPickerButton.color = data.eye_dark_color
-	$Panel/ScrollContainer/VBoxContainer/Eyes/VBoxContainer/ShadowEye/ColorPickerButton.color = data.eye_shadow_color
-	$Panel/ScrollContainer/VBoxContainer/Hair/VBoxContainer/LightHair/ColorPickerButton.color = data.hair_light_color
-	$Panel/ScrollContainer/VBoxContainer/Hair/VBoxContainer/DarkHair/ColorPickerButton.color = data.hair_dark_color
-	$Panel/ScrollContainer/VBoxContainer/Hair/VBoxContainer/OutlineHair/ColorPickerButton.color = data.hair_shadow_color
-	$Panel/ScrollContainer/VBoxContainer/Cloths/VBoxContainer/LightPrimary/ColorPickerButton.color = data.primary_light_color
-	$Panel/ScrollContainer/VBoxContainer/Cloths/VBoxContainer/DarkPrimary/ColorPickerButton.color = data.primary_dark_color
-	$Panel/ScrollContainer/VBoxContainer/Cloths/VBoxContainer/LightSecondary/ColorPickerButton.color = data.secondary_light_color
-	$Panel/ScrollContainer/VBoxContainer/Cloths/VBoxContainer/DarkSecondary/ColorPickerButton.color = data.secondary_dark_color
-	$Panel/ScrollContainer/VBoxContainer/Cloths/VBoxContainer/LightDetail/ColorPickerButton.color = data.detail_light_color
-	$Panel/ScrollContainer/VBoxContainer/Cloths/VBoxContainer/DarkDetail/ColorPickerButton.color = data.detail_dark_color
-
 func store_data():
 	var data:= {
 		"white_color":Color(WHITE_COLOR),
@@ -285,24 +263,6 @@ func _cycle_buffer(inc: int):
 	else:
 		buffer_index = to
 		set_portrait(buffer[to])
-		
-
-func _save():
-	if OS.get_name()=="HTML5":
-		var texture: ViewportTexture = $Viewport.get_texture()
-		var image:= texture.get_data()
-		# warning-ignore:integer_division
-		#HTML5File.save_image(image, "portrait"+str(buffer_index).pad_zeros(3)+".png")
-	else:
-		$FileDialog.show()
-		$FileDialog.invalidate()
-	store_data()
-
-func _save_file(file : String):
-	var texture: ViewportTexture = $Viewport.get_texture()
-	var image:= texture.get_data()
-	image.save_png(file)
-	printt("Saved image to file: "+file)
 
 
 func _set_sprite(ID: int, node: Node2D):
@@ -356,35 +316,6 @@ func _set_brows_above_hair(allowed: bool):
 func _set_brows_offset(value: int):
 	portrait.get_node("Hair/Brows").position.y = -value
 
-
-func add_presets(node):
-	for c in node.get_children():
-		if c is ColorPickerButton:
-			var picker: ColorPicker = c.get_picker()
-			for array in COLORS:
-				for col in array:
-					picker.add_preset(Color(col))
-			picker.connect("preset_added", self, "_add_preset", [self])
-			picker.connect("preset_removed", self, "_remove_preset", [self])
-		else:
-			add_presets(c)
-
-func _add_preset(color, node):
-	for c in node.get_children():
-		if c is ColorPickerButton:
-			var picker: ColorPicker = c.get_picker()
-			picker.add_preset(color)
-		else:
-			_add_preset(color, c)
-
-func _remove_preset(color, node):
-	for c in node.get_children():
-		if c is ColorPickerButton:
-			var picker: ColorPicker = c.get_picker()
-			picker.erase_preset(color)
-		else:
-			_remove_preset(color, c)
-
 func _resized():
 	var scale:= int(max(5.0*min((OS.window_size.x-60)/1024.0, 1.5*(OS.window_size.y-130)/600.0), 1))
 	$Portrait.rect_size = 96*scale*Vector2(1.0,1.0)
@@ -394,56 +325,5 @@ func _ready():
 	buffer.resize(BUFFER_SIZE)
 	_resized()
 	get_tree().connect("screen_resized", self, "_resized")
-	
-	# warning-ignore:shadowed_variable
-	for type in PARTS:
-		if !has_node("Panel/ScrollContainer/VBoxContainer/"+type):
-			var t = type.split("/")
-			if t.size()>1 && has_node("Panel/ScrollContainer/VBoxContainer/"+t[t.size()-1]):
-				for i in range(portrait.get_node(type).Sprites.size()):
-					get_node("Panel/ScrollContainer/VBoxContainer/"+t[t.size()-1]+"/VBoxContainer/HBoxContainer/OptionButton").add_item(portrait.get_node(type).Sprites.keys()[i].capitalize(),i)
-				get_node("Panel/ScrollContainer/VBoxContainer/"+t[t.size()-1]+"/VBoxContainer/HBoxContainer/OptionButton").connect("item_selected", self, "_set_sprite", [portrait.get_node(type)])
-				get_node("Panel/ScrollContainer/VBoxContainer/"+t[t.size()-1]+"/VBoxContainer/HBoxContainer/ButtonLeft").connect("pressed", self, "_cycle", [portrait.get_node(type),-1,get_node("Panel/ScrollContainer/VBoxContainer/"+t[t.size()-1]+"/VBoxContainer/HBoxContainer/OptionButton")])
-				get_node("Panel/ScrollContainer/VBoxContainer/"+t[t.size()-1]+"/VBoxContainer/HBoxContainer/ButtonRight").connect("pressed", self, "_cycle", [portrait.get_node(type),1,get_node("Panel/ScrollContainer/VBoxContainer/"+t[t.size()-1]+"/VBoxContainer/HBoxContainer/OptionButton")])
-			continue
-		for i in range(portrait.get_node(type).Sprites.size()):
-			get_node("Panel/ScrollContainer/VBoxContainer/"+type+"/VBoxContainer/HBoxContainer/OptionButton").add_item(portrait.get_node(type).Sprites.keys()[i].capitalize(),i)
-		get_node("Panel/ScrollContainer/VBoxContainer/"+type+"/VBoxContainer/HBoxContainer/OptionButton").connect("item_selected", self, "_set_sprite", [portrait.get_node(type)])
-		get_node("Panel/ScrollContainer/VBoxContainer/"+type+"/VBoxContainer/HBoxContainer/ButtonLeft").connect("pressed", self, "_cycle", [portrait.get_node(type),-1,get_node("Panel/ScrollContainer/VBoxContainer/"+type+"/VBoxContainer/HBoxContainer/OptionButton")])
-		get_node("Panel/ScrollContainer/VBoxContainer/"+type+"/VBoxContainer/HBoxContainer/ButtonRight").connect("pressed", self, "_cycle", [portrait.get_node(type),1,get_node("Panel/ScrollContainer/VBoxContainer/"+type+"/VBoxContainer/HBoxContainer/OptionButton")])
-	for i in range(TYPES.size()):
-		get_node("Panel/ScrollContainer/VBoxContainer/Type/VBoxContainer/HBoxContainer/OptionButton").add_item(TYPES[i].capitalize(),i)
-		get_node("Panel/ScrollContainer/VBoxContainer/Type/VBoxContainer/HBoxContainer/OptionButton").connect("item_selected", self, "_set_type")
-		get_node("Panel/ScrollContainer/VBoxContainer/Type/VBoxContainer/HBoxContainer/ButtonLeft").connect("pressed", self, "_cycle_type", [-1,get_node("Panel/ScrollContainer/VBoxContainer/Type/VBoxContainer/HBoxContainer/OptionButton")])
-		get_node("Panel/ScrollContainer/VBoxContainer/Type/VBoxContainer/HBoxContainer/ButtonRight").connect("pressed", self, "_cycle_type", [1,get_node("Panel/ScrollContainer/VBoxContainer/Type/VBoxContainer/HBoxContainer/OptionButton")])
-	
-	$Bottom/HBoxContainer/ButtonRng.connect("pressed", self, "_randomize")
-	$Bottom/HBoxContainer/ButtonPrev.connect("pressed", self, "_cycle_buffer", [-1])
-	$Bottom/HBoxContainer/ButtonNext.connect("pressed", self, "_cycle_buffer", [ 1])
-	$Bottom/HBoxContainer/ButtonSave.connect("pressed", self, "_save")
-	$Panel/ScrollContainer/VBoxContainer/Colors/VBoxContainer/Black/ColorPickerButton.connect("color_changed", self, "_set_bw_color", ["black_color"])
-	$Panel/ScrollContainer/VBoxContainer/Colors/VBoxContainer/White/ColorPickerButton.connect("color_changed", self, "_set_bw_color", ["white_color"])
-	$Panel/ScrollContainer/VBoxContainer/Body/VBoxContainer/LightSkin/ColorPickerButton.connect("color_changed", self, "_set_color", ["skin_material","light_color"])
-	$Panel/ScrollContainer/VBoxContainer/Body/VBoxContainer/DarkSkin/ColorPickerButton.connect("color_changed", self, "_set_color", ["skin_material","dark_color"])
-	$Panel/ScrollContainer/VBoxContainer/Eyes/VBoxContainer/LightEye/ColorPickerButton.connect("color_changed", self, "_set_color", ["eye_material","light_color"])
-	$Panel/ScrollContainer/VBoxContainer/Eyes/VBoxContainer/DarkEye/ColorPickerButton.connect("color_changed", self, "_set_color", ["eye_material","dark_color"])
-	$Panel/ScrollContainer/VBoxContainer/Eyes/VBoxContainer/ShadowEye/ColorPickerButton.connect("color_changed", self, "_set_color", ["eye_material","shadow_color"])
-	$Panel/ScrollContainer/VBoxContainer/Hair/VBoxContainer/LightHair/ColorPickerButton.connect("color_changed", self, "_set_color", ["hair_material","light_color"])
-	$Panel/ScrollContainer/VBoxContainer/Hair/VBoxContainer/DarkHair/ColorPickerButton.connect("color_changed", self, "_set_color", ["hair_material","dark_color"])
-	$Panel/ScrollContainer/VBoxContainer/Hair/VBoxContainer/OutlineHair/ColorPickerButton.connect("color_changed", self, "_set_color", ["hair_material","shadow_color"])
-	$Panel/ScrollContainer/VBoxContainer/Cloths/VBoxContainer/LightPrimary/ColorPickerButton.connect("color_changed", self, "_set_color", ["primary_material","light_color"])
-	$Panel/ScrollContainer/VBoxContainer/Cloths/VBoxContainer/DarkPrimary/ColorPickerButton.connect("color_changed", self, "_set_color", ["primary_material","dark_color"])
-	$Panel/ScrollContainer/VBoxContainer/Cloths/VBoxContainer/LightSecondary/ColorPickerButton.connect("color_changed", self, "_set_color", ["secondary_material","light_color"])
-	$Panel/ScrollContainer/VBoxContainer/Cloths/VBoxContainer/DarkSecondary/ColorPickerButton.connect("color_changed", self, "_set_color", ["secondary_material","dark_color"])
-	$Panel/ScrollContainer/VBoxContainer/Cloths/VBoxContainer/LightDetail/ColorPickerButton.connect("color_changed", self, "_set_color", ["detail_material","light_color"])
-	$Panel/ScrollContainer/VBoxContainer/Cloths/VBoxContainer/DarkDetail/ColorPickerButton.connect("color_changed", self, "_set_color", ["detail_material","dark_color"])
-	$Panel/ScrollContainer/VBoxContainer/Neck/VBoxContainer/CheckBox.connect("toggled", self, "_set_neck_above_cloths")
-	$Panel/ScrollContainer/VBoxContainer/Eyes/VBoxContainer/CheckBox.connect("toggled", self, "_set_eyes_above_hair")
-	$Panel/ScrollContainer/VBoxContainer/Brows/VBoxContainer/CheckBox.connect("toggled", self, "_set_brows_above_hair")
-	$Panel/ScrollContainer/VBoxContainer/Brows/VBoxContainer/Offset/HSlider.connect("value_changed", self, "_set_brows_offset")
-	$FileDialog.connect("file_selected", self, "_save_file")
-	$FileDialog.current_dir = OS.get_system_dir(OS.SYSTEM_DIR_PICTURES)
-	
-	add_presets($Panel/ScrollContainer/VBoxContainer)
 	
 	_randomize()
