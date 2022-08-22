2922bd1454100c45e134d5462674e0b5b4e53c42
sebastiap <sebastiap@gmail.com>
2022-08-22 20:17:42 -0300

    Traffic Jam V2

--- SpaceshipHarem/LevelComponents/Segments/TrafficJamSegment.gd
+++ SpaceshipHarem/LevelComponents/Segments/TrafficJamSegment.gd
@@ -2,19 +2,26 @@ extends Segment
 
 onready var timer = $SegmentTime
 
+
+#Backlog
+#Las diagonales solo para dificultad 5 o mas
+# Bocinazos y ruidos de trafico
+# Domingueros e Ira de Carretera para algunos autos
+
 var spawner_scene = preload("res://LevelComponents/Spawner.tscn")
 var primary_enemy_scene = preload("res://Enemies/TrafficCar.tscn")
-#var secondary_enemy_scene = preload("res://Enemies/Enemy.tscn")
-#var tertiary_enemy_scene = preload("res://Enemies/Enemy2.tscn")
 
 var rng = RandomNumberGenerator.new()
 
 var types = [
-[{"xspeed": -70 - difficulty*3,"cadence":100,"rotation_degrees": 0},[150,-50,50,50]], # Derecha a izq
-[{"xspeed": 70  + difficulty*3,"cadence":100,"rotation_degrees": 180},[300,650,50,50]], #  Izq a Der
-[{"yspeed": 70 + difficulty*3,"xspeed": 10 ,"cadence":100,"rotation_degrees": 90},[550,550,50,50]], # Hacia Arriba
-[{"yspeed": -70  -difficulty*3,"xspeed": 10 ,"cadence":100,"rotation_degrees": 270},[-150,50,50,50]], # Hacia Abajo
-[{"yspeed": -70  -difficulty*3,"xspeed": 10 ,"cadence":100,"rotation_degrees": 270},[-150,50,50,50]]
+[{"xspeed": -70 - difficulty*3,"cadence":100,"rotation_degrees": 0},[150,-50,50,50],"DI"], # Derecha a izq
+[{"xspeed": 70  + difficulty*3,"cadence":100,"rotation_degrees": 180},[300,650,50,50],"ID"], #  Izq a Der
+[{"yspeed": 70 + difficulty*3,"xspeed": 10 ,"cadence":100,"rotation_degrees": 90},[550,550,50,50],"HAR"], # Hacia Arriba
+[{"yspeed": -70  -difficulty*3,"xspeed": 10 ,"cadence":100,"rotation_degrees": 270},[-50,50,50,50],"HAB"], # Hacia Abajo
+[{"yspeed": -70  -difficulty*3,"xspeed": -70 ,"cadence":100,"rotation_degrees": 45},[-50,-50,50,50],"DIAG1"],
+[{"yspeed": -70  -difficulty*3,"xspeed": 70 ,"cadence":100,"rotation_degrees": 135},[-50,550,50,50],"DIAG2"],
+[{"yspeed": 70  -difficulty*3,"xspeed": 70 ,"cadence":100,"rotation_degrees": 225},[400,650,50,50],"DIAG3"],
+[{"yspeed": 70  -difficulty*3,"xspeed": -70 ,"cadence":100,"rotation_degrees": 315},[400,-50,50,50],"DIAG4"]
 ] # Hacia Abajo
 
 
@@ -23,19 +30,26 @@ func start_segment():
 	var ways = types.size()
 	var previous_ways = []
 	var rng = RandomNumberGenerator.new()
+	var way_passed = 3
 	rng.randomize()
-	for i in difficulty /3 :
+	
+
+	for i in 3 :
 		print("Vuelta")
-		var way = types[rng.randi_range(0, types.size() - 1)]
-		while previous_ways.has(way):
-			way = types[rng.randi_range(0, types.size() - 1)]
-			print("Repetido")
-		
-		add_traffic_line(time/3,3,primary_enemy_scene,way)
-		previous_ways.append(way)
-	#add_enemy_group(time/3,time/3,secondary_enemy_scene)
-	#add_enemy_group(time/3,time * 2/3,tertiary_enemy_scene)
-	timer.start(time)
+		for j in difficulty /3 :
+			var way = types[rng.randi_range(0, types.size() - 1)]
+			var way_id = way[2]
+			while previous_ways.has(way_id):
+				way = types[rng.randi_range(0, types.size() - 1)]
+				way_id = way[2]
+				print("Repetido")
+			previous_ways.append(way_id)
+			add_traffic_line(time/3,way_passed,primary_enemy_scene,way)
+		#add_traffic_line(time/3,time/3,primary_enemy_scene,way)
+		#add_enemy_group(time/3,time * 2/3,primary_enemy_scene,way)
+		way_passed += time/3
+		previous_ways.clear()
+	timer.start(time)	
 	
 func _on_SegmentTime_timeout():
 	end_segment()
@@ -47,7 +61,7 @@ func add_traffic_line(duration,start,enemy,line):
 	var spawner = spawner_scene.instance()
 	spawner.scene = enemy
 	var vars = line[0]
-	#{"yspeed": 70 + difficulty*3,"xspeed": 10 ,"cadence":100,"rotation_degrees": 90}# Abajo
+
 	spawner.scene_variables = vars
 	
 
